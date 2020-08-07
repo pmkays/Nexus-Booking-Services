5923b119daeb9c36c56dc07ba318ba70658d5515
Ian Nguyen S3788210 <s3788210@student.rmit.edu.au>
2020-08-07 16:49:23 +1000

    Base project set up

---
+++ BackEnd/SEPT.Team.Seven/src/main/java/SEPT/Team/Seven/Application.java
@@ -0,0 +1,13 @@
+package SEPT.Team.Seven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
