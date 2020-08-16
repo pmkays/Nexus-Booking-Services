4b39a6b7745860c7368dcf2d200a34fe0aa25d5c
Paula Kurniawan s3782041 <s3782041@student.rmit.edu.au>
2020-08-16 12:43:58 +1000

    Removed untested front-end for release

--- FrontEnd/src/constants/images.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path='./images.d.ts'/>
-
-// import image from '../assets/images/someImage.png';
-
-export {
-  // image
-};
--- FrontEnd/src/store/configureStore.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore as configureStoreProd } from './configureStore.prod';
-import { configureStore as configureStoreDev } from './configureStore.dev';
-
-const configureStore =  process.env.NODE_ENV === 'prod' ? configureStoreProd : configureStoreDev;
-
-export { configureStore };
