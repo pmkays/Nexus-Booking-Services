3640cc290b6f0195f48216a756138cc657daef94
davidkevork <davidkevork@gmail.com>
2020-08-26 11:50:33 +1000

    Add HowItWorks page

--- FrontEnd/appointment-system/src/App.js
+++ FrontEnd/appointment-system/src/App.js
@@ -10,6 +10,7 @@ import EditProfile from "./containers/Profile/EditProfile/EditProfile";
 import AboutUs from "./components/AboutUs/AboutUs";
 import ContactUs from "./components/ContactUs/ContactUs";
 import * as actions from "./store/actions/actions";
+import HowItWorks from "./components/HowItWorks/HowItWorks";
 
 
 class App extends Component {
@@ -29,6 +30,7 @@ class App extends Component {
           <Route path="/logout" component={Logout} />
           <Route path="/about" component={AboutUs} />
           <Route path="/contact" component={ContactUs} />
+          <Route path="/howitworks" component={HowItWorks} />
           <Route path="/" render={() => <h1>Welcome to the Nexus Appointment System</h1>} />
         </Switch>
       );
@@ -39,6 +41,7 @@ class App extends Component {
           <Route path="/logout" component={Logout} />
           <Route path="/about" component={AboutUs} />
           <Route path="/contact" component={ContactUs} />
+          <Route path="/howitworks" component={HowItWorks} />
           <Route path="/" render={() => <h1>Welcome to the Nexus Appointment System</h1>} />
         </Switch>
       );
---
+++ FrontEnd/appointment-system/src/components/HowItWorks/HowItWorks.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { redirect } from "../../utility/history";
+
+const HowItWorks = (props) => {
+  return (
+    <div>
+      <br />
+      <h2 className="text-center">The booking System of your dreams</h2>
+      <hr/>
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HowItWorks;
