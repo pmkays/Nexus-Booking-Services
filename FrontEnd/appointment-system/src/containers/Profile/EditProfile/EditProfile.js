5ec8ee574f370fea8d83c20c5d0acd957b92047a
Ian Nguyen <s3788210@student.rmit.edu.au>
2020-09-06 22:18:57 +1000

    Added validation to name

--- FrontEnd/appointment-system/src/containers/Profile/EditProfile/EditProfile.js
+++ FrontEnd/appointment-system/src/containers/Profile/EditProfile/EditProfile.js
@@ -22,6 +22,7 @@ export class EditProfile extends Component{
             value: this.props.profileDetails == null ? "" : this.props.profileDetails.firstName,
             validation: {
               required: true,
+              isLetters: true
             },
             valid: true,
             touched: false,
@@ -36,6 +37,7 @@ export class EditProfile extends Component{
             value: this.props.profileDetails == null ? "" : this.props.profileDetails.lastName,
             validation: {
               required: true,
+              isLetters: true
             },
             valid: true,
             touched: false,
