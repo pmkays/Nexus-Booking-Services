9c985f530cfa87cf4911683c9fdf78daec124ca1
Ian Nguyen <s3788210@student.rmit.edu.au>
2020-09-06 22:15:46 +1000

    Added sign up button

--- FrontEnd/appointment-system/src/containers/HowItWorks/HowItWorks.js
+++ FrontEnd/appointment-system/src/containers/HowItWorks/HowItWorks.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const HowItWorks = (props) => {
+   const redirectRegister = () => {
+    props.history.push("/register");
+  };
+
   return (
     <div>
       <br />
@@ -15,6 +19,11 @@ const HowItWorks = (props) => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
+      </div>
+    <div className="d-flex justify-content-center">
+        <button className="btn btn-link text-center" onClick={redirectRegister}>
+         Sign Up
+        </button>
       </div>
     </div>
   );
