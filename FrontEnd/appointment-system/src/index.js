9618427615a04ad55c5db40d8c0ba547ccca7a72
Ian Nguyen <s3788210@student.rmit.edu.au>
2020-09-22 18:08:45 +1000

    Update index.js

--- FrontEnd/appointment-system/src/index.js
+++ FrontEnd/appointment-system/src/index.js
@@ -17,7 +17,7 @@ import bookingReducer from "./store/reducers/booking";
 const composeEnhancers =
   process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-    : null;
+    : compose;
 
 const rootReducer = combineReducers({
   auth: authReducer,
