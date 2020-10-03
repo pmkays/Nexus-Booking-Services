71fa67bdef75d10d09153bb4e6062f5d161d844d
Josiah Miranda <josiahmiranda@hotmail.com>
2020-10-03 23:32:04 +1000

    Multiple bookings can be made on a day now

--- BackEnd/SEPT.Team.Seven/src/main/java/SEPT/Team/Seven/service/BookingService.java
+++ BackEnd/SEPT.Team.Seven/src/main/java/SEPT/Team/Seven/service/BookingService.java
@@ -24,8 +24,8 @@ public class BookingService {
 	private CustomerRepository customerRepository;
 	private ServiceRepository serviceRepository;
 
-	public BookingService(BookingRepository bookingRepository, WorkingTimeRepository workingTimeRepository, 
-			EmployeeRepository employeeRepository, CustomerRepository customerRepository, 
+	public BookingService(BookingRepository bookingRepository, WorkingTimeRepository workingTimeRepository,
+			EmployeeRepository employeeRepository, CustomerRepository customerRepository,
 			ServiceRepository serviceRepository) {
 		this.workingTimeRepository = workingTimeRepository;
 		this.employeeRepository = employeeRepository;
@@ -44,79 +44,87 @@ public class BookingService {
 		return bookings;
 	}
 
-	public Optional<Booking> addBooking(int employeeId, int customerId, Date startTime, Date endTime,
-			int serviceId) {
-		
+	public Optional<Booking> addBooking(int employeeId, int customerId, Date startTime, Date endTime, int serviceId) {
+
 		System.out.println(startTime);
 		System.out.println(endTime);
-		
+
 		if (startTime == null || endTime == null) {
 			return Optional.empty();
 		}
-		
+
 		if (!serviceRepository.findById(serviceId).isPresent()) {
 			return Optional.empty();
 		}
 
 		// ensure that the customer and employee actually exist
-		if (employeeRepository.findById(employeeId).isPresent() &&
-				customerRepository.findById(customerId).isPresent()) {
-			
-			// now check that a customer doesn't have a booking on that day already			
+		if (employeeRepository.findById(employeeId).isPresent()
+				&& customerRepository.findById(customerId).isPresent()) {
+
+			// now check that a customer doesn't have a booking on that day already that
+			// overlaps
 			List<Booking> customersBookings = bookingRepository.findAllByCustomerId(customerId);
 			Calendar newStartCal = Calendar.getInstance();
 			newStartCal.setTime(startTime);
 			Calendar newEndCal = Calendar.getInstance();
 			newEndCal.setTime(endTime);
-			
+
 			for (Booking booking : customersBookings) {
 				Calendar cal = Calendar.getInstance();
+				Calendar endCal = Calendar.getInstance();
 				cal.setTime(booking.getStartTime());
-				
-				if (cal.get(Calendar.DATE) == newStartCal.get(Calendar.DATE) &&
-						cal.get(Calendar.MONTH) == newStartCal.get(Calendar.MONTH) &&
-						cal.get(Calendar.YEAR) == newStartCal.get(Calendar.YEAR)) {
-					return Optional.empty();
+				endCal.setTime(booking.getEndTime());
+
+				if (cal.get(Calendar.DATE) == newStartCal.get(Calendar.DATE)
+						&& cal.get(Calendar.MONTH) == newStartCal.get(Calendar.MONTH)
+						&& cal.get(Calendar.YEAR) == newStartCal.get(Calendar.YEAR)) {
+					// now we have to check that the times dont overlap
+					// so this is checking that the new start is after the booking is done,
+					// or it ends before the booking starts.
+					if (!(newStartCal.compareTo(endCal) >= 0 || newEndCal.compareTo(cal) <= 0)) {
+						// so if these conditions are not meant, we'll return empty.
+						System.out.println("OVERLAPS WITH ANOTHER BOOKING");
+						return Optional.empty();
+					}
 				}
 			}
-			
+
 			// now check that the booking is within the employee's working times
 			List<WorkingTime> employeesWorkingTimes = workingTimeRepository.findAllByEmployeeId(employeeId);
-			
+
 			for (WorkingTime workingTime : employeesWorkingTimes) {
 				Calendar workingTimeStart = Calendar.getInstance();
 				workingTimeStart.setTime(workingTime.getStartTime());
 				Calendar workingTimeEnd = Calendar.getInstance();
 				workingTimeEnd.setTime(workingTime.getEndTime());
-				
+
 				// The employee has a working time on this date
-				if (workingTimeStart.get(Calendar.DATE) == newStartCal.get(Calendar.DATE) &&
-						workingTimeStart.get(Calendar.MONTH) == newStartCal.get(Calendar.MONTH) &&
-						workingTimeStart.get(Calendar.YEAR) == newStartCal.get(Calendar.YEAR)) {
-					
+				if (workingTimeStart.get(Calendar.DATE) == newStartCal.get(Calendar.DATE)
+						&& workingTimeStart.get(Calendar.MONTH) == newStartCal.get(Calendar.MONTH)
+						&& workingTimeStart.get(Calendar.YEAR) == newStartCal.get(Calendar.YEAR)) {
+
 					if (newStartCal.before(newEndCal)) {
-						
+
 						// Check that the booking is actually within the working time
-						boolean startValid = newStartCal.compareTo(workingTimeStart) >= 0 && newStartCal.compareTo(workingTimeEnd) <= 0;
-						boolean endValid = newEndCal.compareTo(workingTimeStart) >= 0 && newEndCal.compareTo(workingTimeEnd) <= 0;
-						
+						boolean startValid = newStartCal.compareTo(workingTimeStart) >= 0
+								&& newStartCal.compareTo(workingTimeEnd) <= 0;
+						boolean endValid = newEndCal.compareTo(workingTimeStart) >= 0
+								&& newEndCal.compareTo(workingTimeEnd) <= 0;
+
 						if (startValid && endValid) {
-							return Optional.of(bookingRepository.save(
-									new Booking(customerRepository.findById(customerId).get(),
-											employeeRepository.findById(employeeId).get(),
-											startTime, endTime, "accepted",
-											serviceRepository.findById(serviceId).get())));
-							
+							return Optional.of(
+									bookingRepository.save(new Booking(customerRepository.findById(customerId).get(),
+											employeeRepository.findById(employeeId).get(), startTime, endTime,
+											"accepted", serviceRepository.findById(serviceId).get())));
+
 						}
-						
+
 					}
-					
+
 				}
-				
-				
+
 			}
-			
-			
+
 		}
 
 		return Optional.empty();
