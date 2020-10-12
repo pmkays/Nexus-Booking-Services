5e038dd3993b3c01a1c98a12aabdc0e147c1ffa8
Josiah Miranda <josiahmiranda@hotmail.com>
2020-10-13 00:07:01 +1100

    Fixed booking test

--- BackEnd/SEPT.Team.Seven/src/main/java/SEPT/Team/Seven/CommandLineAppStartupRunner.java
+++ BackEnd/SEPT.Team.Seven/src/main/java/SEPT/Team/Seven/CommandLineAppStartupRunner.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import SEPT.Team.Seven.model.Availability;
